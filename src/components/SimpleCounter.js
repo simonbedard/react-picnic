ab194cfeea1260d7490b9aa079e2b1d8518c820d
Simon Bédard <77559758+simonbedard@users.noreply.github.com>
2021-05-03 21:31:42 -0400

    Update simple counter component

--- src/components/SimpleCounter.js
+++ src/components/SimpleCounter.js
@@ -1,37 +1,65 @@
-import React, { Component } from 'react'
+import React, { useEffect, useState } from 'react';
 
-export default class SimpleCounter extends Component {
+function SimpleCounter() {
 
-    constructor(props){
-        super(props);
+    const [count, setCount] = useState(0);
+    const [status, setStatus] = useState(0);
 
-        this.addOne = this.addOne.bind(this);
-        this.minusOne = this.minusOne.bind(this);
+    const loopTime = 4;
 
-        this.state = {
-            count: 0
-        };
-       
+    // Call when count is changing
+    useEffect(() => {
+        updateStatus();
+    }, [count])   
+     
+    function plus(time) {
+        let newCount = count + time
+        setCount(newCount)
     }
+    function minus(time){
+        let newCount = count - time
+        setCount(newCount)
+    }
+    function devide(time){
+        let newCount = count / time
+        setCount(newCount)
 
-    addOne(){
-        this.setState({
-            count : this.state.count + 1
-        });
     }
-    minusOne(){
-        this.setState({
-            count : this.state.count - 1
-        });
+    function multiple(time){
+        let newCount = count * time
+        setCount(newCount)
     }
-    render() {
-        return (
-            <div>
-                <h2>Simple counter</h2>
-                <button onClick={this.minusOne} type='button'>-1</button>
-                <code>{this.state.count}</code>
-                <button onClick={this.addOne} type='button'>+1</button>
-            </div>
-        )
+    function updateStatus() {
+        if(count > 0){
+            setStatus('positive')
+        }else if(count < 0){
+            setStatus('negative')
+        }else{
+            setStatus('')
+        }
     }
+    
+    return (
+        <div>
+            <h2>Simple counter</h2>
+            <code className={`counter ${status}`}>{count}</code>
+            <br /> <br />
+            {Array.from({ length: loopTime }, (_, k) => (
+                <button key={k}onClick={() => {minus(k+1)}} type='button'>-{k+1}</button>
+            ))}
+            <button className="multi-dev" onClick={() => setCount(0)} type='button'>Reset</button>
+
+            {Array.from({ length: loopTime }, (_, k) => (
+                <button key={k}onClick={() => {plus(k+1)}} type='button'>+{k+1}</button>
+            ))}
+            <br /> <br />
+            <button className="multi-dev" onClick={() => {devide(2)}} type='button'>Devide by 2</button>
+            <button className="multi-dev" onClick={() => {multiple(2)}} type='button'>Multiple by 2</button>
+
+        </div>
+    );
+    
 }
+
+
+export default SimpleCounter;
